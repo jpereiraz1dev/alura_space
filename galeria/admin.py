97825d847e8e2aa34f7cff743fafbc98b43d1f0e
Joao Pedro <jpereiraz.dev@gmail.com>
2025-09-08 20:48:32 -0300

    segundo curso finalizado

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from galeria.models import Fotografia
 
-# Register your models here.
+
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id","nome","legenda", "publicada") #* oq aparece no display do site
+    list_display_links = ("id","nome") #* os links que levam a edicao
+    search_fields = ("nome",) #* cria uma busca
+    list_filter = ("categoria",) #* cria um filtro
+    list_editable = ("publicada",)
+    list_per_page = 10
+    
+
+admin.site.register(Fotografia, ListandoFotografias)
